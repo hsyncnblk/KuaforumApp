fccf990a20e9318352481e4f71f4797af348bd52
huseyin cinibulak <huseyincnblk@gmail.com>
2023-12-22 01:16:43 +0300

    user firebase authentication completed

--- app/src/main/java/com/hsyncnblk/kuaforumapp/LoginFragment.kt
+++ app/src/main/java/com/hsyncnblk/kuaforumapp/LoginFragment.kt
@@ -5,19 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.hsyncnblk.kuaforumapp.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
 
     private lateinit var binding: FragmentLoginBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLoginBinding.inflate(inflater,container,false)
+
 
         binding.buttonLoginKayitOl.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.login_to_signUp)
@@ -28,9 +34,50 @@ class LoginFragment : Fragment() {
 
         }
 
-
-
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+
+        val currentUser = auth.currentUser
+
+        if (currentUser != null){
+            Navigation.findNavController(requireView()).navigate(R.id.login_to_musteriMain)
+        }
+
+        binding.buttonLoginGirisYap.setOnClickListener {
+            val email = binding.etMusteriUsername.text.toString()
+            val password = binding.etMusteriPassword.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()){
+                auth.signInWithEmailAndPassword(email,password).addOnCompleteListener {task->
+                    if (task.isSuccessful){
+                        Navigation.findNavController(requireView()).navigate(R.id.login_to_musteriMain)
+                    }else{
+                        Toast.makeText(context,"${task.exception?.localizedMessage}", Toast.LENGTH_SHORT).show()
+
+                    }
+
+                }.addOnFailureListener {
+                    Toast.makeText(context,it.localizedMessage, Toast.LENGTH_SHORT).show()
+
+                }
+            }else{
+                Toast.makeText(context,"Boş alan olmamalı!", Toast.LENGTH_SHORT).show()
+
+            }
+
+
+        }
+
+
+
     }
 
 
--- app/src/main/java/com/hsyncnblk/kuaforumapp/SignUpFragment.kt
+++ app/src/main/java/com/hsyncnblk/kuaforumapp/SignUpFragment.kt
@@ -5,12 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.Navigation
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.hsyncnblk.kuaforumapp.databinding.FragmentSignUpBinding
 
 
 class SignUpFragment : Fragment() {
 
     private lateinit var binding: FragmentSignUpBinding
+    private lateinit var auth: FirebaseAuth
 
 
     override fun onCreateView(
@@ -18,6 +24,44 @@ class SignUpFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding= FragmentSignUpBinding.inflate(inflater,container,false)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+
+
+        binding.buttonSignUpKayitOl.setOnClickListener {
+
+            val email = binding.etMusteriKayitUsername.text.toString()
+            val password = binding.etMusteriKayitPassword.text.toString()
+            val confirmPassword = binding.etMusteriKayitPasswordTekrar.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()){
+                if (password == confirmPassword){
+
+                    auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener {
+
+                        Navigation.findNavController(requireView()).navigate(R.id.signUp_to_musteriMain)
+
+                    }.addOnFailureListener {
+                        Toast.makeText(context,it.localizedMessage,Toast.LENGTH_SHORT).show()
+
+                    }
+
+                }else{
+                    Toast.makeText(context,"Parolanız aynı değil!",Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(context,"Boş alan olmamalı!!",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+
+
+
+
+
         return binding.root
     }
 
