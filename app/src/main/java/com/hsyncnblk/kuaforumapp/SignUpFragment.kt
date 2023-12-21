46af43382f26468ba74a9f6c6fa6de84baeb48aa
huseyin cinibulak <huseyincnblk@gmail.com>
2023-12-21 22:50:05 +0300

    kuafor login-signup completed

---
+++ app/src/main/java/com/hsyncnblk/kuaforumapp/SignUpFragment.kt
@@ -0,0 +1,25 @@
+package com.hsyncnblk.kuaforumapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hsyncnblk.kuaforumapp.databinding.FragmentSignUpBinding
+
+
+class SignUpFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignUpBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding= FragmentSignUpBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+
+}
